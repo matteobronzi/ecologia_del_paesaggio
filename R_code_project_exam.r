ee5c81aadcb040e1fe53fbdd7451098ed88cb20f
matteobronzi <62288680+matteobronzi@users.noreply.github.com>
2020-06-11 12:39:45 +0200

    Create R_code_project_exam.r

---
+++ R_code_project_exam.r
@@ -0,0 +1,25 @@
+
+### codice R progetto per esame di ecologia del paesaggio 
+
+# pacchetti utilizzati 
+
+library(raster)
+
+setwd("E:/ecuador")
+
+# carico su R le immagini scaricate da Copernicus dell'indice NDVI mondiale del 2014 e del 2020 (risoluzione 300m)
+
+ndvi2020 <- raster("c_gls_NDVI300_202002010000_GLOBE_PROBAV_V1.0.1.nc")
+ndvi2014 <- raster("c_gls_NDVI300_201402010000_GLOBE_PROBAV_V1.0.1.nc")
+
+# ritaglio la porzione dell'immagine relativa al Sud America con la funzione 
+
+ext <- c(-80,0,-50,0)
+
+ecuador2014 <- crop(ndvi2014, ext)
+ecuador2020 <- crop(ndvi2020, ext)
+
+par(mfrow=c(1,2))
+plot(ecuador2014)
+plot(ecuador2020)
+
